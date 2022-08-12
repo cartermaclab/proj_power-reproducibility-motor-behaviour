998957c9ed2ad440b6ffb7e66aed26892e3eb534
Carter Lab <cartem11@mcmaster.ca>
2022-08-11 20:05:43 -0400

    init

---
+++ comparisons/script.R
@@ -0,0 +1,4 @@
+
+
+accuracy_diff <- daff::diff_data(dat_mb, dat_bm)
+daff::render_diff(accuracy_diff)
---
+++ scripts/libraries.R
@@ -0,0 +1,20 @@
+#> -------------------------------------------
+#> ON THE REPRODUCIBILITY OF POWER ANALYSES
+#> -- McKay, Bacelar, and Carter
+#>
+#> Required libraries
+#>
+#> Authors:
+#>   Brad McKay
+#>
+#> Last update: July 31 2022
+#>
+#> Website: https://www.cartermaclab.org
+#> -------------------------------------------
+
+#> SCRIPT SETUP ----
+#>
+#> REQUIRED LIBRARIES
+library(tidyverse)
+library(waffle)
+library(patchwork)
